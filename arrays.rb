a2c2a47b9ab1025d3e77c1e7a84140f7a662e34e
Matt Enyeart <menyeart1@gmail.com>
2022-11-02 12:39:40 -0600

    Add answers to the array methods question

--- arrays.rb
+++ arrays.rb
@@ -6,3 +6,14 @@ floats = [1.5, 2.5, 3.5, 4.5]
 
 booleans = [true, false, false, true, false]
 
+# Removes the last element of the strings array and return the removed element.
+strings.pop
+
+# Adds the specified new element to the end of the integers array and return the modified array.
+integers.push(6)
+
+# Removes the first element of the shift array and returns the removed element.
+floats.shift
+
+# Adds the new specified element to the beginning of the booleans array and returns the modified array.
+booleans.unshift(false)
