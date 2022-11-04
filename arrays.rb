3a69df98b3ea3c2c1d1eaf26c9dcd8ab3f0192cb
Matt Enyeart <menyeart1@gmail.com>
2022-11-04 11:20:01 -0600

    Correct mistakes in the arrays exercise

--- arrays.rb
+++ arrays.rb
@@ -8,34 +8,38 @@ booleans = [true, false, false, true, false]
 
 # Removes the last element of the strings array and return the removed element.
 strings.pop
+# Returns "pear"
 
 # Adds the specified new element to the end of the integers array and return the modified array.
 integers.push(6)
+# Returns [1, 2, 3, 4, 5, 6]
 
 # Removes the first element of the shift array and returns the removed element.
 floats.shift
+# Returns 1.5
 
 # Adds the new specified element to the beginning of the booleans array and returns the modified array.
 booleans.unshift(false)
+# Returns [false, true, false, false, true, false]
 
 #Index Postions
 
-# Four elements in the strings array
-strings.length = 4
+# Returns the number of elements (4) int he strings array, 
+strings.length
 
-# The first element's position in the string array 0
-strings[0] = "apple"
+# The first element's position in the string array is 0
+strings[0]
 
 # The fourth element's position in the string array is 3
-strings[3] = "pear"
+strings[3]
 
 # Array first and last methods
 
-#Array first method will return the first element of an array
-strings.first = "apple"
+#Array first method will return the first element ("apple) of an array
+strings.first
 
-#Array last method will return the last element of an array
-strings.last = "pear"
+#Array last method will return the last element ("pear") of an array
+strings.last
 
 
 
